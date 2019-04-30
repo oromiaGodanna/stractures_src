0ef8094e4ca9d9ed2c531185383be9cd9b0332e7
Oromia Godanna <ori.gsero@gmail.com>
2019-04-30 10:42:45 +0300

    Added getter for Organizations list

--- src/app/organizations/organization.service.ts
+++ src/app/organizations/organization.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { Organization } from './organization.model';
+import { Organizations } from './organization-mock';
+import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class OrganizationService {
 
+  getOrganizations(): Observable<Organization[]>{
+    return of(Organizations);
+  }
   constructor() { }
 }
