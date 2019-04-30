368764230740a6ec4e1ecb705409114d2d4d6853
Oromia Godanna <ori.gsero@gmail.com>
2019-04-30 10:35:58 +0300

    Added moke organizations data

---
+++ src/app/organizations/organization-mock.ts
@@ -0,0 +1,9 @@
+import { Organization } from './organization.model';
+
+export const Organizations: Organization [] =[
+    new Organization("Organization Name 1", "org01", "private", "Description about this organization"),
+    new Organization("Organization Name 2", "org02", "private", "Description about this organization"),
+    new Organization("Organization Name 3", "org03", "private", "Description about this organization"),
+    new Organization("Organization Name 4", "org04", "private", "Description about this organization"),
+    new Organization("Organization Name 5", "org05", "private", "Description about this organization")
+]
