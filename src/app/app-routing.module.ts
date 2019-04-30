7131f458513b5879d14785916a32304f82944ed3
Oromia Godanna <ori.gsero@gmail.com>
2019-04-30 15:00:18 +0300

    Added routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DepartmentsListComponent } from './departments/departments-list/departments-list.component';
+import { DepartmentDetailComponent } from './departments/department-detail/department-detail.component';
+import { DepartmentNewComponent } from './departments/department-new/department-new.component';
+import { DepartmentAlterComponent } from './departments/department-alter/department-alter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'departments', pathMatch: 'full'},
+  { path: 'departments/:orgId', component: DepartmentsListComponent},
+  { path: 'detail/:name', component: DepartmentDetailComponent },
+  { path: 'new', component : DepartmentNewComponent},
+  { path: 'edit/:name', component : DepartmentAlterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
