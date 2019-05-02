ea7845daa4176b2c99b261d34f11f1857f8c704b
Oromia Godanna <ori.gsero@gmail.com>
2019-05-02 23:14:35 +0300

    department model added

---
+++ src/app/departments/department.model.ts
@@ -0,0 +1,19 @@
+export class Department {
+    public id:number;
+    public orgId: string;
+    public parentId: number;
+    public name: string;
+    public description: string;
+
+    
+    
+
+    constructor(id:number, orgId:string, parentId:number, name: string, desc: string) {
+        this.id=id;
+        this.orgId = orgId;
+        this.parentId = parentId;
+        this.name = name;
+        this.description = desc;
+        
+    }
+}
