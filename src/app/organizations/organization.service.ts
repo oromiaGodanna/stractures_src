9f26eaebd8697c744a550b333ee300f5f3ccb8e2
Oromia Godanna <ori.gsero@gmail.com>
2019-05-06 10:59:40 +0300

    Added Delete organization function

--- src/app/organizations/organization.service.ts
+++ src/app/organizations/organization.service.ts
@@ -2,13 +2,26 @@ import { Injectable } from '@angular/core';
 import { Organization } from './organization.model';
 import { Organizations } from './organization-mock';
 import { Observable, of } from 'rxjs';
+import { R3FactoryDelegateType } from '@angular/compiler/src/render3/r3_factory';
 @Injectable({
   providedIn: 'root'
 })
 export class OrganizationService {
+  AllOrganizations = Organizations;
+  
 
   getOrganizations(): Observable<Organization[]>{
     return of(Organizations);
+  }
+  getOrganization(orgId: string): Organization{ 
+    return this.AllOrganizations.find(organization => organization.orgId == orgId);
+    
+  }
+  deleteOrganization(id: string){
+    var index = this.AllOrganizations.indexOf(this.getOrganization(id));
+    this.AllOrganizations.splice(index, 1);
+    //this.departmentService.deleteAllOrganizationDepartments(id);
+    //this.organizationListChanged.next(this.orgnizations.slice());
   }
   constructor() { }
 }
