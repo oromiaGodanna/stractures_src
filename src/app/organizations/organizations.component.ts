b3a03e62e6b593c26f0fee4a26fde34aa9208d48
Oromia Godanna <ori.gsero@gmail.com>
2019-05-06 10:58:33 +0300

    Delete component added for organizations list

--- src/app/organizations/organizations.component.ts
+++ src/app/organizations/organizations.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Organization } from './organization.model';
 import { OrganizationService } from './organization.service';
+import { NzMessageService } from 'ng-zorro-antd';
+
 @Component({
   selector: 'app-organizations',
   templateUrl: './organizations.component.html',
@@ -11,7 +13,7 @@ export class OrganizationsComponent implements OnInit {
   organization_list : Organization[];
   selectedOrgId : string;
   
-  constructor(private organizationService: OrganizationService) { }
+  constructor(private organizationService: OrganizationService, private nzMessageService: NzMessageService) { }
 
   ngOnInit() {
     this.getOrganizations();
@@ -19,9 +21,22 @@ export class OrganizationsComponent implements OnInit {
 
   getOrganizations(): void{
     this.organizationService.getOrganizations().subscribe(organization_list => this.organization_list = organization_list)
+    console.log(this.organization_list);
   }
   getId(orgId: string): void{
     console.log(orgId);
     this.selectedOrgId = orgId;
   }
+  onDelete(): void{
+    
+    this.organizationService.deleteOrganization(this.selectedOrgId);
+    this.nzMessageService.info('Organization Deleted Successfully. ');
+  }
+  onCancel(): void {
+    this.nzMessageService.info('Task canceled.');
+  }
+
+  
+   
+  
 }
