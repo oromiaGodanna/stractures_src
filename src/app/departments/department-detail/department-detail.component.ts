3e8a4e0852406eef5f144efd0db96dd68d38bc4e
Oromia Godanna <ori.gsero@gmail.com>
2019-05-08 17:31:03 +0300

    Views added for data fetched(the detail) in service

--- src/app/departments/department-detail/department-detail.component.ts
+++ src/app/departments/department-detail/department-detail.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Department } from '../department.model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { Location } from '@angular/common';
 import { DepartmentService } from '../department.service';
+import { NzMessageService } from 'ng-zorro-antd';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-department-detail',
@@ -9,21 +12,61 @@ import { DepartmentService } from '../department.service';
   styleUrls: ['./department-detail.component.css']
 })
 export class DepartmentDetailComponent implements OnInit {
-  @Input() department:Department;
+
+  name:string;
+  
+  department: Department;
+  parentName: String;
+  deptsUnder: Department[];
+
   constructor(private route: ActivatedRoute,
-    private departmentService: DepartmentService
+    private departmentService: DepartmentService,
+    private router: Router,
+    private location: Location,
+    private nzMessageService: NzMessageService
     ) { }
   ngOnInit() {
-    this.getDepartment();
+
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.name = params['name'];
+        this.department = this.departmentService.getDep(this.name);
+        this.parentName = this.departmentService.getManagingDepartment(this.department.parentId);
+        this.getDepartmentsUnder(this.department.id);
+      });
+    
+   
+    console.log(this.deptsUnder)
 }
 
-getDepartment(): void {
-  const name = this.route.snapshot.paramMap.get('name');
-  this.departmentService.getDepartment(name).subscribe(department => this.department = department);
-}
-
-// goBack(): void {
-//   this.location.back();
+// getDepartment(): void {
+//   const name = this.route.snapshot.paramMap.get('name');
+//   this.name = name;
+//   this.departmentService.getDepartment(name).subscribe(department => this.department = department);
+//   this.department = this.departmentService.getDep(name);
 // }
 
+getManagingDept(parentId: number){
+
+  this.parentName = this.departmentService.getManagingDepartment(parentId);
+  return this.parentName;
+}
+getDepartmentsUnder(deptId: number){
+  this.deptsUnder = this.departmentService.getDepartmentsUnder(deptId);
+}
+goBack(): void {
+  this.location.back();
+}
+
+
+  onDelete(): void{
+    
+    this.departmentService.deleteDepartment(this.name);
+    this.nzMessageService.info('Organization Deleted Successfully. ');
+    this.router.navigate(['departments', this.department.orgId]);
+    
+  }
+  onCancel(): void {
+    this.nzMessageService.info('Task canceled.');
+  }
 }
