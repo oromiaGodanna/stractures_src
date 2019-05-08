aa8131c14dae237deb92e0b5bca1105b0118fa49
Oromia Godanna <ori.gsero@gmail.com>
2019-05-08 17:25:46 +0300

    Delete department method added

--- src/app/departments/department.service.ts
+++ src/app/departments/department.service.ts
@@ -10,11 +10,31 @@ export class DepartmentService {
 
   constructor() { }
 
+  allDepartments = Departments;
+  children: Department[] = [];
+
   getDepartments(): Observable<Department[]> {
     return of(Departments);
   }
   getDepartment(name: string): Observable<Department> {
     return of(Departments.find(department => department.name === name));
   }
-  
+  getChildren(id: number): Department[]{
+   
+    this.allDepartments.forEach(department => {
+      if(department.parentId === id){
+        this.children.push(department);
+        console.log(department.name);
+      }
+
+    });
+    return this.children;
+  }
+  getDep(name: string): Department {
+    return this.allDepartments.find(department => department.name === name);
+  }
+  deleteDepartment(name: string){
+    var index = this.allDepartments.indexOf(this.getDep(name));
+    this.allDepartments.splice(index, 1);
+  }
 }
