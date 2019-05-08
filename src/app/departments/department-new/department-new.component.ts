abc44560e871769060bbce8911d20ba71212a0d0
Oromia Godanna <ori.gsero@gmail.com>
2019-05-08 17:29:29 +0300

    New department form added with controls

--- src/app/departments/department-new/department-new.component.ts
+++ src/app/departments/department-new/department-new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+//import { formControlBinding } from '@angular/forms/src/directives/ng_model';
 
 @Component({
   selector: 'app-department-new',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentNewComponent implements OnInit {
 
+  alterForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.alterForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required)
+    });
+    
+  }
+  onSubmit(){
+    var name = this.alterForm.value['name'];
+    var description = this.alterForm.value['description'];
+    console.log(name);
+    console.log(description);
+   
   }
 
 }
