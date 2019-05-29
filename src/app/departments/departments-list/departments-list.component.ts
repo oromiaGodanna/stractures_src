944b166b63a918258e19cf282beb682f03ba438e
Oromia Godanna <ori.gsero@gmail.com>
2019-05-29 21:17:54 +0300

    Tree view corrected

--- src/app/departments/departments-list/departments-list.component.ts
+++ src/app/departments/departments-list/departments-list.component.ts
@@ -6,7 +6,7 @@ import { Department } from '../department.model';
 import { OrganizationService } from '../../organizations/organization.service';
 import { map, filter } from 'rxjs/operators'
 import { Observable } from 'rxjs';
-import { NzMessageService } from 'ng-zorro-antd';
+import { NzMessageService, NzFormatEmitEvent } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-departments-list',
@@ -16,14 +16,15 @@ import { NzMessageService } from 'ng-zorro-antd';
 export class DepartmentsListComponent implements OnInit {
 
    orgId: string;
-   tableView = 'true';
+   tableView ;
    AllDepartments: Department[];
    orgDepts: Department[] = [];
-   children : Department[];
+   children : Department[] = [];
    nodes = [];
    root : Department;
    selectedDeptId:number;
    orgName: string;
+   childrenDetails: any[] = [];
   constructor(private route: ActivatedRoute,
     private departmentService: DepartmentService,
     private organizationService: OrganizationService,
@@ -33,10 +34,11 @@ export class DepartmentsListComponent implements OnInit {
 
   ngOnInit() {
     this.getDepartments();
+    this.tableView =  true;
     this.getOrg();
-   // this.getTreeStracture();
+   this.getTreeStracture();
     //console.log(this.orgDepts);
-    this.tableView =  "true";
+    
   }
 
   getDepartments():any{
@@ -56,41 +58,46 @@ export class DepartmentsListComponent implements OnInit {
    this.orgName = org.name;
   }
   checkChild(departmentId: number){
-    this.children = this.departmentService.getChildren(departmentId);
-    console.log(this.children);
-    if(this.children != null){
-      this.getChildren();
-    }else{
-      return this.children
-    }
+    
+    var children = [];
+    var childDepts = [];
+    this.orgDepts.forEach(dept => {
+      
+    if(dept.parentId == departmentId ){
+        childDepts.push(dept);
+      }
+    });
+    if(childDepts != null){
+      childDepts.forEach(dept => {
+        children.push({
+            title: dept.name,
+            key: dept.id,
+            expanded: true,
+            children: this.checkChild(dept.id),
+            isLeaf: (this.checkChild(dept.id).length < 1) ? true : false
+          });
+
+        });
+      }
+    return children;
+
   }
 
-  
-  getChildren(){
-    this.children.forEach(child =>{
-      return [{
-        title: child.name,
-        key: child.id,
-        expanded: true,
-        children: this.checkChild(child.id),
-        isLeaf: (this.children.length < 1) ? true : false
-      }]
-    })
-  }
   getTreeStracture(){
-    this.root = this.orgDepts.find(department => department.parentId === 0);
+    var CEO = this.orgDepts.find(department => department.name == 'CEO');
+    this.root = this.orgDepts.find(department => department.parentId === CEO.parentId);
     
     this.nodes.push({
       title: this.root.name,
       key: this.root.id,
       expanded: true,
       children: this.checkChild(this.root.id),
-      isLeaf: (this.children.length < 1) ? true : false
+      isLeaf: (this.checkChild(this.root.id)  == null) ? true : false
 
       });
       console.log(this.nodes);
 
-      }
+  }
       getId(Id:number){
         this.selectedDeptId = Id;
         console.log(this.selectedDeptId);
@@ -104,9 +111,20 @@ export class DepartmentsListComponent implements OnInit {
       onCancel(): void {
         this.nzMessageService.info('Task canceled.');
       }
-      
+      nzClick(event: NzFormatEmitEvent): void {
+        var dept;
+        var node_key = event.node.key;
+        this.orgDepts.forEach(element => {
+          if(element.id == parseInt(node_key)){
+            this.router.navigate(["/detail", element.id]);
+
+          }
+        });
+        
+      }
   
     }
+    
 
     // this.orgDepts.forEach(department => {
     //   departmentIds.push(department.id);  
